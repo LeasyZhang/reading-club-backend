35b703e3d16379604b42c7725cb7a16008acbae0
joe zhang <joe.zhang@ringcentral.com>
2019-06-19 15:25:06 +0800

    fix error null pointer problem

--- database/dao/history/borrowHistoryDao.go
+++ database/dao/history/borrowHistoryDao.go
@@ -30,28 +30,21 @@ func SaveOrUpdate(history *entity.BorrowHistory) {
 func GetUserBorrowedHistory(userName string, bookID int) (entity.BorrowHistory, *dto.HistoryErrorResponse) {
 
 	var bookHistory entity.BorrowHistory
-	var errorRsp dto.HistoryErrorResponse
 
 	db, err = database.GetDBConnection()
 
 	if err != nil {
-		errorRsp.Error = constant.CanNotConnectDatabase
-		errorRsp.ErrorCode = constant.CanNotConnectDatabaseCode
-		return bookHistory, &errorRsp
+		return bookHistory, &dto.HistoryErrorResponse{ErrorCode: constant.CanNotConnectDatabaseCode, Error: constant.CanNotConnectDatabase}
 	}
 
 	bookErros := db.Where("user_name = ? and book_id = ? and history_status = ?", userName, bookID, constant.BookFree).First(&bookHistory).GetErrors()
 
 	for _, err := range bookErros {
 		if gorm.IsRecordNotFoundError(err) {
-			errorRsp.Error = constant.HistoryNotFound
-			errorRsp.ErrorCode = constant.HistoryNotFoundCode
-			return bookHistory, &errorRsp
+			return bookHistory, &dto.HistoryErrorResponse{ErrorCode: constant.HistoryNotFoundCode, Error: constant.HistoryNotFound}
 		}
 
-		errorRsp.Error = constant.InternalServerError
-		errorRsp.ErrorCode = constant.InternalServerErrorCode
-		return bookHistory, &errorRsp
+		return bookHistory, &dto.HistoryErrorResponse{ErrorCode: constant.InternalServerErrorCode, Error: constant.InternalServerError}
 	}
 
 	return bookHistory, nil
