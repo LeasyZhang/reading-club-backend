f8487f4ec313572cea1655c35c9a1255e2bb3b8f
joe zhang <joe.zhang@ringcentral.com>
2019-06-21 09:43:37 +0800

    add-log

--- middleware/jwtHandler.go
+++ middleware/jwtHandler.go
@@ -56,6 +56,7 @@ func AuthMiddleWare() (*jwt.GinJWTMiddleware, error) {
 				fmt.Println(err)
 				return "", jwt.ErrMissingLoginValues
 			}
+			fmt.Println(loginVals)
 			userID := loginVals.Username
 			password := loginVals.Password
 
