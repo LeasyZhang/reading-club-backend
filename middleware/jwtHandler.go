2665abebe1dc0fc605a8e5fcd2cb889707ad8d90
joe zhang <joe.zhang@ringcentral.com>
2019-07-28 14:14:53 +0800

    add custom login handler, return user role

--- middleware/jwtHandler.go
+++ middleware/jwtHandler.go
@@ -37,8 +37,8 @@ func AuthMiddleWare() (*jwt.GinJWTMiddleware, error) {
 	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
 		Realm:       "test zone",
 		Key:         []byte("secret key"),
-		Timeout:     10,
-		MaxRefresh:  30,
+		Timeout:     3 * time.Minute,
+		MaxRefresh:  24 * time.Hour,
 		IdentityKey: identityKey,
 		PayloadFunc: func(data interface{}) jwt.MapClaims {
 			if v, ok := data.(*User); ok {
